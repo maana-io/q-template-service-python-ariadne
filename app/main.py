235e099e7fd7318db439795a2d9f83e8704200b0
Logan Gore <lgore@maana.io>
2020-01-02 23:30:21 -0800

    Add support for env vars, auth, graphql client, and client injected into graphql context.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,62 @@
 from ariadne import ObjectType, QueryType, MutationType, gql, make_executable_schema
 from ariadne.asgi import GraphQL
 from asgi_lifespan import Lifespan, LifespanMiddleware
+from graphqlclient import GraphQLClient
+
+# HTTP request library for access token call
+import requests
+# .env
+from dotenv import load_dotenv
+import os
+
+# Load environment variables
+load_dotenv()
+
+
+def getAuthToken():
+    authProvider = os.getenv('AUTH_PROVIDER')
+    authDomain = os.getenv('AUTH_DOMAIN')
+    authClientId = os.getenv('AUTH_CLIENT_ID')
+    authSecret = os.getenv('AUTH_SECRET')
+    authIdentifier = os.getenv('AUTH_IDENTIFIER')
+
+    url = ''
+    if authProvider == 'keycloak':
+        url = f'{authDomain}/auth/realms/{authIdentifier}/protocol/openid-connect/token'
+    else:
+        url = f'https://{authDomain}/oauth/token'
+
+    payload = {
+        'grant_type': 'client_credentials',
+        'client_id': authClientId,
+        'client_secret': authSecret,
+        'audience:': authIdentifier
+    }
+
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+
+    r = requests.post(url, data=payload, headers=headers)
+    response_data = r.json()
+    print("Finished auth token request...")
+    return response_data['access_token']
+
+
+def getClient():
+    graphqlClient = None
+
+    # Build as closure to keep scope clean.
+
+    def buildClient(client=graphqlClient):
+        # Cached in regular use cases.
+        if (client is None):
+            print('Building graphql client...')
+            token = getAuthToken()
+            url = os.getenv('MAANA_ENDPOINT_URL')
+            client = GraphQLClient(url)
+            client.inject_token('Bearer '+token)
+        return client
+    return buildClient()
+
 
 # Define types using Schema Definition Language (https://graphql.org/learn/schema/)
 # Wrapping string in gql function provides validation and better error traceback
@@ -22,7 +78,23 @@ query = QueryType()
 
 # Resolvers are simple python functions
 @query.field("people")
-def resolve_people(*_):
+def resolve_people(_, info):
+
+    # # A resolver can access the graphql client via the context.
+    # client = info.context["client"]
+
+    # # Query all maana services.
+    # result = client.execute('''
+    # {
+    #     allServices {
+    #         id
+    #         name
+    #     }
+    # }
+    # ''')
+
+    # print(result)
+
     return [
         {"firstName": "Marie", "lastName": "Curie", "age": 21},
         {"firstName": "Rubab", "lastName": "Nedzo", "age": 24},
@@ -44,7 +116,9 @@ schema = make_executable_schema(type_defs, [query, person])
 # --- ASGI app
 
 # Create an ASGI app using the schema, running in debug mode
-app = GraphQL(schema, debug=True)
+# Set context with authenticated graphql client.
+app = GraphQL(
+    schema, debug=True, context_value={'client': getClient()})
 
 # 'Lifespan' is a standalone ASGI app.
 # It implements the lifespan protocol,
